522ad0fd576a4b930033483e866add3456dbd1ea
lamtev178 <lamtev2000@gmail.com>
2023-06-02 11:28:47 +0300

    fix: test

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -3,5 +3,5 @@ const getFederationTypesUrl = require("../cli");
 test("simple url", () => {
   expect(
     getFederationTypesUrl("app1@http://localhost:3001/remoteEntry.js")
-  ).toBe("http://localhost:3001/federated-types/app1");
+  ).toBe("http://localhost:3001/federated-types/app1.d.ts");
 });
