4a86dd2653b1e1f23c94293692b7da89ef311d7f
lamtev178 <lamtev2000@gmail.com>
2023-06-05 13:49:28 +0300

    fix prettier quotes

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,7 @@ module.exports = {
   tabWidth: 2,
   useTabs: false,
   semi: true,
-  doubleQuote: true,
+  singleQuote: false,
   quoteProps: "as-needed",
   trailingComma: "all",
   bracketSpacing: true,
