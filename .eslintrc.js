8e3c424eb44fc023a57b60646eb69333378adce7
lamtev178 <lamtev2000@gmail.com>
2023-06-05 14:37:14 +0300

    ci: add eslint check

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,9 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {
+    "@typescript-eslint/no-var-requires": "off",
+    "no-undef": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+  },
 };
